3a2813c6a703916cf272fd285c813940b360b58e
Anna Tindall <act_96@hotmail.co.uk>
2023-12-06 11:12:46 +0000

    Day 6 part 1: Maths

--- src/day06.rs
+++ src/day06.rs
@@ -1,8 +1,41 @@
 pub fn day06(input_lines: &str) -> (String, String) {
-    let _ = input_lines;
-    let answer1 = 0;
+    let races = read_races(input_lines);
+    let answer1: u32 = races.iter().map(Race::count_record_breakers).product();
     let answer2 = 0;
     (format!("{}", answer1), format!("{}", answer2))
+}
+
+fn read_races(input_lines: &str) -> Vec<Race> {
+    let mut lines = input_lines.lines();
+
+    let times = lines.next().unwrap().split_ascii_whitespace().skip(1).map(|t| t.parse::<u32>().unwrap());
+    let distances = lines.next().unwrap().split_ascii_whitespace().skip(1).map(|t| t.parse::<u32>().unwrap());
+
+    times.zip(distances).map(|(t,d)| {
+        Race { record_mm: d, time_ms: t }
+    }).collect()
+}
+
+struct Race {
+    record_mm: u32,
+    time_ms: u32
+}
+
+impl Race {
+    fn count_record_breakers(&self) -> u32 {
+        // Solve quadratic inequality:
+        // x^2 - rx + d < 0
+        // x = button time
+        // r = race time
+        // d = record distance
+        let discriminant: f64 = (self.time_ms * self.time_ms - 4*self.record_mm).into();
+        let upper_limit = (self.time_ms as f64 + discriminant.sqrt()) / 2.0;
+        let mut lower_limit = (self.time_ms as f64 - discriminant.sqrt()) / 2.0;
+        if lower_limit < 0.0 {
+            lower_limit = 0.0;
+        }
+        (upper_limit.floor() - lower_limit.floor()) as u32
+    }
 }
 
 #[cfg(test)]
@@ -10,8 +43,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn check_day06_part1_case1() {
-        assert_eq!(day06("").0, "0".to_string())
+    fn check_record_breakers() {
+        let race = Race {
+            record_mm: 9,
+            time_ms: 7
+        };
+        assert_eq!(race.count_record_breakers(), 4);
     }
 
     #[test]
